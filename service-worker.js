3eb614f5b3379ba63299f1f1ce1772b3313edaaf
vyshnavi461 <vyshuvyshnavi251@gmail.com>
2026-06-20 14:05:19 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "expense-tracker-v1";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./manifest.json"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
